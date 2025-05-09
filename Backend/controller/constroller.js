1d92c774e4d0259a929bac1e3452b7dd709df4d1
jaswin9360 <jaswinkumar00@gamil.com>
2025-05-09 10:56:17 +0530

    adding new changes

--- Backend/controller/constroller.js
+++ Backend/controller/constroller.js
@@ -1,11 +1,11 @@
 const jwt = require('jsonwebtoken');
-const users =require("../mongoose");
+const users = require("../mongoose");
 const bcrypt = require("bcrypt")
 const detail = require("../mongodb")
 
 
  const home =async (req,res)=>{
-const user = await detail.find()
+  const user = await detail.find({},{password:1,username:1,_id:0})
    res.send(user)
 }
 
@@ -32,12 +32,12 @@ const login = async (req,res)=>{
    const {username,password}=req.body;
    console.log(username,password)
     try{
-        const userExist=await detail.findOne({username})
+        const userExist=await detail.findOne({username,password})
         console.log(userExist)
         if(!userExist){
-          return   res.send({message:"User Not Found"})
+          return   res.json({message:"User Not Found" })
         }
-        if(password===userExist.password){
+        if(password===userExist.password && username ===userExist.username){
          return res.send({message:"Login Successfully"})
      }
      const compare = await bcrypt.compare(password,detail.password)
--- Backend/route/routes.js
+++ Backend/route/routes.js
@@ -3,7 +3,7 @@ const route = express.Router()
 const {home,login,register}=require("../controller/constroller")
 const auth = require("../middleware/auth")
 
-route.get ("/home",auth,home)
+route.get ("/home",home)
 route.post("/register",register)
 route.post("/login",login)
 
